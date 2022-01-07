4d116f92f6547401bfd47bb4774c478f1b422de8
André Ascensão <andre.ascensao@bitsighttech.com>
2022-01-07 03:30:47 +0000

    index calculator

--- index.js
+++ index.js
@@ -3,22 +3,28 @@ const moment = require("moment");
 const fs = require("fs");
 
 const COIN_LIMIT = 10; // max: 5000
-const FIRST_DATE = "2013-04-28"; // min: 2013-04-28
+const FIRST_DATE = "2022-01-05"; // min: 2013-04-28
+const OFFSET = 0;
 
 const createCSV = (historicalData) => {
   const cryptoList = Object.keys(historicalData);
   const columns = ["Date", ...cryptoList];
   let priceRows = [];
   let dominanceRows = [];
+  let indexRows = [];
   let date = moment(FIRST_DATE);
   while (date < moment().startOf("day")) {
     const currentDate = date.format("YYYY-MM-DD");
     let priceRow = [currentDate];
     let dominanceRow = [currentDate];
+    let indexRow = [currentDate, 0];
     cryptoList.forEach((coin) => {
       if (historicalData[coin][currentDate]) {
         priceRow.push(historicalData[coin][currentDate].price);
         dominanceRow.push(historicalData[coin][currentDate].dominance);
+        indexRow[1] +=
+          historicalData[coin][currentDate].price *
+          historicalData[coin][currentDate].dominance;
       } else {
         priceRow.push("");
         dominanceRow.push("");
@@ -26,6 +32,7 @@ const createCSV = (historicalData) => {
     });
     priceRows.push(priceRow);
     dominanceRows.push(dominanceRow);
+    indexRows.push(indexRow);
     date = date.add(1, "days");
   }
 
@@ -50,13 +57,25 @@ const createCSV = (historicalData) => {
   fs.writeFile("./output/dominance_sheet.csv", dominanceSheet, (err) => {
     if (err) return console.log(err);
   });
+  const indexSheet = [["Date", "Index"], ...indexRows.reverse()].reduce(
+    (acc, item) => {
+      acc += item.toString() + "\n";
+      return acc;
+    },
+    ""
+  );
+  fs.writeFile("./output/index_sheet.csv", indexSheet, (err) => {
+    if (err) return console.log(err);
+  });
 };
 
 let historicalData = {};
 const getData = (date) =>
   axios
     .get(
-      `https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/historical?convert=EUR&date=${date}&limit=${COIN_LIMIT}`
+      `https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/historical?convert=EUR&date=${date}&limit=${COIN_LIMIT}&start=${
+        OFFSET + 1
+      }`
     )
     .then((response) => {
       console.log(date);
@@ -73,7 +92,9 @@ const getData = (date) =>
       });
       const nextDate = moment(date).add(1, "days");
       if (nextDate < moment().startOf("day")) {
-        getData(nextDate.format("YYYY-MM-DD"));
+        setTimeout(() => {
+          getData(nextDate.format("YYYY-MM-DD"));
+        }, 1000);
       } else {
         createCSV(historicalData);
       }
